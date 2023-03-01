e2244d86cbe8df2a937647edcb81d8ee5e5aa398
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2023-03-01 21:30:06 +0100

    correcting CSS and adding JS

--- src/js/main.js
+++ src/js/main.js
@@ -17,4 +17,29 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
+const buttonAdd = document.querySelector('.button--add__js');
+const buttonDelete = document.querySelector('.button--delete__js');
+const counter = document.querySelector('.glass--number__js');
+const key = new Date().toISOString().slice(0,10);
 
+
+if(!localStorage.getItem(key)){
+  localStorage.setItem(key, 0);
+  counter.innerHTML = "0";
+} else{
+  counter.innerHTML = localStorage.getItem(key);
+}
+
+
+buttonAdd.addEventListener('click', (e) => {
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) + 1);
+  counter.innerHTML = parseInt(counter.innerHTML) + 1;
+})
+
+buttonDelete.addEventListener('click', (e) => {
+  const counterValue = parseInt(counter.innerHTML);
+  if (counterValue > 0){
+  localStorage.setItem(key, parseInt(localStorage.getItem(key)) - 1);
+  counter.innerHTML = counterValue - 1;
+  }
+})
